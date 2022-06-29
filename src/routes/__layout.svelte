0045643e44f3d90060d3ccd64d2ddbaa4c2a8bac
Vladimir Porotnikov <zerdox.cool4@gmail.com>
2022-06-30 01:39:59 +0500

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	let test = 0;
+</script>
+
+<div>
+	<button on:click={() => test++}>
+		{ test }
+	</button>
+</div>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<div on:click|stopPropagation class="dialog-content">
+	<a href="/test">i am broken link with page refresh</a>
+</div>
+
+<a href="/test">i am working link with no page refresh</a>
